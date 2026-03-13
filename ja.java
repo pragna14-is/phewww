6969e3db8e23e3919692fbf8503a216b86119514
pragna14-is <pragnahemalatha363@gmail.com>
2026-03-13 13:23:32 +0530

    Update ja.java

--- ja.java
+++ ja.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Welcome to Java programming!");
          System.out.println("Hello, World!");
          System.out.println("Welcome to Java programming!");
+         System.out.println("Welcome to Java programming!");
     }
 }
